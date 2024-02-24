cc428029a255c00d6652ea11866cb16560c2d111
Jeff Rey <jeff.rey4973@gmail.com>
2024-02-24 15:06:49 +0100

    ADD env and docerfile

--- a_config/settings.py
+++ a_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u-q9vpi6+36groi+qq_8qgb#lqdve#__ep9iskvb*ygnkczide
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.178.48', 'localhost']
 
 
 # Application definition
